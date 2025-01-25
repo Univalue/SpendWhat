d1d936f6fcd2ff38d85c2319d52fc987d34a574a
YongHuM1ng <1277651550@qq.com>
2025-01-25 16:50:03 +0800

    更新

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { ModeWatcher, mode } from "mode-watcher";
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <!-- 深色模式主题 -->
+    <meta name="theme-color" content={$mode === "light" ? "#ffffff" : "#000000"}>
+    <title></title>
+</svelte:head>
+
+<ModeWatcher />
+{@render children()}
